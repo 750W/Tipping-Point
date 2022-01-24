4fe6f43dcd4ff27d8646e8ef9a497f2008b1fc52
samysrini05 <samysrini2005@gmail.com>
2022-01-24 10:27:09 -0500

    added in auton selex

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ void initialize() {
   imu_x.reset();
   imu_z.reset();
   //Task tracker_task(acceleration_tracker,(void*)"not needed",TASK_PRIORITY_DEFAULT,TASK_STACK_DEPTH_DEFAULT,"tracker task");
-  //auton_selector();
+  auton_selector();
   Task drive(drive_funct, (void*)"not needed", "driver control task");
 }
 
